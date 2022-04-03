6f53c2b94f2223892d79be694f65e7665f3116e7
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-04-03 19:23:30 +0900

    initial commit

---
+++ SwiftCLI/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  
+//  
+//  Created by kunikuni03 on 2022/04/03
+//  
+//
+
+import Foundation
+
+let args = CommandLine.arguments.dropFirst()
+print("commandline argument: \(args)")
+
+let filePath = FileManager.default.currentDirectoryPath
+print("current directory: \(filePath)")
